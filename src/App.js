858fc54d68972d7374673293d8f854826c507426
robonexx <robertwagar@gmail.com>
2022-07-29 07:07:50 +0200

    base project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home';
+import Page2 from './pages/Page2/Page2';
+import Page3 from './pages/Page3/Page3';
+import Page4 from './pages/Page4/Page4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/page2' element={<Page2 />} />
+        <Route path='/page3' element={<Page3 />} />
+        <Route path='/page4' element={<Page4 />} />
+      </Routes>
+     
     </div>
   );
 }
